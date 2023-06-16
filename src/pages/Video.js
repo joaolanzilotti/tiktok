4a0d50c587bd3f0ccaf967c50773dc7c00a01b63
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-06-16 14:44:27 -0300

    Implemented Footer Component and Install new Dependency Material UI

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import "./video.css";
+import VideoFooter from "./components/footer/VideoFooter";
 
 function Video() {
 
@@ -28,6 +29,8 @@ function Video() {
             src='https://media.istockphoto.com/id/1385148464/pt/v%C3%ADdeo/cute-ginger-cat-with-glasses-sitting-in-a-box-on-a-yellow-background-in-neon-light.mp4?s=mp4-640x640-is&k=20&c=UG8HHhPcPU_UED_yAh7EU6fAYj9tkLjPs1DajcV--vQ=' className="video__player">
                 
             </video>
+            {/* Side Bar */}
+            <VideoFooter/>
         </div>
     )
 }
