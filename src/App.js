bd9252444f1d37859349fe2ba8c2c3d1f9bf7de5
joaolanzilotti <algamerjoao1@hotmail.com>
2023-06-17 20:46:29 -0300

    Finalized Frontend and components

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
      <div className='app__videos'>
-        <Video/>
-        <Video/>
-        <Video/>
-        <Video/>
+        <Video likes={25} messages={200} shares={300} name="Joao Pedro" description="Brecker o goleiro" music="Musica Epica" url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"/>
+         <Video likes={65} messages={200} shares={300}  name="Paulo" description="Brecker o goleiro" music="Musica Epica" url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/bird.mp4?alt=media&token=52abbeec-ff95-4acb-808e-5a4b977d1da"/>
+         <Video likes={656} messages={200} shares={300}  name="Paulo" description="Brecker o goleiro" music="Musica Epica" url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"/>
+         <Video likes={12000} messages={200} shares={300}  name="Paulo" description="Brecker o goleiro" music="Musica Epica" url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"/>
 
      </div>
     </div>
--- src/pages/components/sidebar/VideoSideBar.js
+++ src/pages/components/sidebar/VideoSideBar.js
@@ -4,7 +4,7 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ChatIcon from '@mui/icons-material/Chat';
 import ShareIcon from '@mui/icons-material/Share';
-function VideoSideBar() {
+function VideoSideBar({likes, messages, shares}) {
 
     const [liked, setLiked] = useState(false);
     function handdleLike(){
@@ -14,15 +14,15 @@ function VideoSideBar() {
         <div className="videoSideBar" onClick={handdleLike}>
             <div className="videoSideBar__options">
                 { liked ? <FavoriteIcon fontSize="large" /> :  <FavoriteBorderIcon fontSize="large" /> }
-               <p> { liked ? 300 + 1 : 300 } </p>
+               <p> { liked ? likes + 1 : likes } </p>
             </div>
             <div className="videoSideBar__options">
                <ChatIcon fontSize="large" />
-               <p>200</p>
+                <p> { messages } </p>
             </div>
             <div className="videoSideBar__options">
                <ShareIcon fontSize="large" />
-               <p>100</p>
+                <p> { shares } </p>
             </div>
         </div>
     );
